206a8340c54d2f664121ba2a20860b27641c7a0f
Somya Bansal <somya@Somya.PC>
2018-02-03 12:15:25 +0530

    add broadcast

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
 	// console.log('a user connected');
 	socket.on('chat message', function(msg){
-		console.log('message:  ' + msg);
+		io.emit('chat message', msg);
+		// console.log('message:  ' + msg);
 	});
 });
 
